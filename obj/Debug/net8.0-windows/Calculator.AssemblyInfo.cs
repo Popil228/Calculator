2d9f7638ca8dce482c4942d8baf6f249d5307261
Haleniuk.M <galenukm@gmail.com>
2025-06-06 15:16:27 +0300

    Calculatore 1.1

--- obj/Debug/net8.0-windows/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+729df04cbcae7d8541d975ae40c2484c7cfee881")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
